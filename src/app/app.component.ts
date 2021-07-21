703e2a053951bd5a713b8d9ddf76b6b440db523b
Carlos Aguilar <caguilar@tiresia.co>
2021-07-21 17:01:45 -0500

    PWA-config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
+import {DataService} from './data.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(updates:SwUpdate, private data:DataService){
+    updates.available.subscribe(event =>{
+      // IF THRES AN UPDATE ON THE SERVICE WORKER IT WILL UPDATE
+      updates.activateUpdate().then(()=>document.location.reload());
+    })
+  }
+
+  joke:any;
+  update:boolean = false;
+
   title = 'angular-pwa';
+
+  ngOnInit(){
+    this.data.giveMeJokes().subscribe(res=>{
+      this.joke = res;
+    })
+  }
+
 }
